8a7698328e2d7373c2c9be653d17c775300b4c8a
PS <poosoni@apac.corpdir.net>
2022-07-07 17:49:55 +0530

    girl in office

---
+++ script.js
@@ -0,0 +1,17 @@
+// Poonam Soni - All rights reserved
+
+// Copyright © 2021 Poonam Soni
+
+setInterval(() => {
+    const todayDate = new Date();
+    const hourTime = todayDate.getHours();
+    const minuteTime = todayDate.getMinutes();
+    const secondTime = todayDate.getSeconds();
+    const hRotation = 30 * hourTime + minuteTime/2;
+    const mRotation = 6 * minuteTime;
+    const sRotation = 6 * secondTime;
+    hour.style.transform = `rotate(${hRotation}deg)`;
+    minute.style.transform = `rotate(${mRotation}deg)`;
+    second.style.transform = `rotate(${sRotation}deg)`;
+    
+}, 1000);
